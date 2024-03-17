95dfc4ea033ccf8085ef229ba5095966bc8914b4
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-03-17 11:48:43 +0000

    count from db

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,28 +1,60 @@
+"use client"
 import DashboardCard from "@/components/cards/dashboard-card";
 import StaffChart from "@/components/containers/staff-chart";
 import { dashBoardIconsMap } from "@/utils/client";
+import { useQuery } from "@tanstack/react-query";
+import CustomError from "@/components/core/custom-error";
+import CustomLoader from "@/components/loaders/custom-loader";
+import { GET_ALL_COUNTS } from "@/utils/server/public";
 
 export default function Home() {
-  
+  const { isPending, isError, data, error, isSuccess } = useQuery({
+    queryKey: ['all-counts'],
+    queryFn: async () => {
+      const rooms = await GET_ALL_COUNTS();
+      return rooms;
+
+    },
+    retry: 3,
+    staleTime: 300,
+    refetchOnMount: true
+  });
+
+  if (isPending) {
+    return (
+      <section className="flex items-center justify-center w-full h-full">
+        <CustomLoader />
+      </section>
+
+    );
+  }
+  if (isError || data === undefined) {
+    return (
+      <section className="flex items-center justify-center w-full h-full">
+        <CustomError />
+      </section>
+
+    );
+  }
   return (
     <main className="flex  flex-col items-center gap-10 px-2">
       <section className="grid gap-4  w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <DashboardCard
           label="Total Staff"
-          value={400}
+          value={data?.staff || 0}
           icon={dashBoardIconsMap.staff}
           link="/staff"
         />
         <DashboardCard
           label="Total Messages"
-          value={1000}
+          value={data?.communications || 0}
           icon={dashBoardIconsMap.messages}
           link="/messages"
 
         />
         <DashboardCard
           label="Total Patients"
-          value={2000}
+          value={data?.patients}
           icon={dashBoardIconsMap.patients}
           link="/blog"
 
---
+++ src/utils/server/public.ts
@@ -0,0 +1,28 @@
+import { ApiResponse, ChangePasswordInput, LoginUserInput, Pagination, ResetPasswordInput, SendCodeInput, UpdateUserDetailsInput, User, UserRes, UserRoles, VerifyCodeInput } from "@/types";
+import Axios from "../axios";
+import _ from "lodash";
+
+export type AllCounts = {
+    staff: number;
+    bookings: number;
+    communications: number;
+    patients: number;
+};
+
+
+export const GET_ALL_COUNTS = async () => {
+    try {
+        const response: ApiResponse<AllCounts> = await Axios({
+            method: "GET",
+            url: `/public/`,
+        });
+
+        if (response.data?.success) {
+            return response.data.data;
+        } else {
+            throw new Error(response?.data?.message);
+        }
+    } catch (error) {
+        throw error;
+    }
+};
